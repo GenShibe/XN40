d637fbef9d71cce79a9be56be48dfc1f57e170be
Gen <buzhybrid@gmail.com>
2024-04-09 00:42:29 -0700

    add flake files

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Gen's NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+   home-manager = {
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
+     };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+  let
+  system = "x86_64-linux";
+  pkgs = nixpkgs.legacyPackages.${system};
+   in
+   {
+ nixosConfigurations = {
+  default = nixpkgs.lib.nixosSystem {
+      system = "XN40";
+       specialArgs = {
+            inherit inputs;
+          };
+        modules = [
+        { _module.args = { inherit inputs; }; }
+                {
+         nixpkgs.config.allowUnfree = true;
+        }
+        ./configuration.nix      inputs.home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+          useGlobalPkgs = true;
+           extraSpecialArgs = {
+            inherit inputs;
+          };
+          users."gen".imports = [ ./home.nix ];
+         };
+        }
+      ];
+    };
+    };
+    };
+    }
